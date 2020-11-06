205173373313a026a1542f62ad79e67144edda88
keiichirotakahashi <k16takahashi125@gmail.com>
2020-11-07 03:12:39 +0900

    Create a devise user

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.6.5'
 
 # base
 gem 'bootsnap', '>= 1.4.2', require: false
+gem 'devise'
 gem 'dotenv-rails'
 gem 'jbuilder', '~> 2.7'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
@@ -31,6 +32,7 @@ group :development, :test do
 end
 
 group :development do
+  gem 'annotate'
   gem 'listen', '~> 3.2'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  # User
+  devise_for :users, skip: [:registrations, :sessions]
+  devise_scope :user do
+    get '/signup', to: 'users/registrations#new'
+    post '/signup', to: 'users/registrations#create'
+    get '/login', to: 'users/sessions#new'
+    post '/login', to: 'users/sessions#create'
+    delete '/logout', to: 'users/sessions#destroy'
+  end
   root 'home#index'
 end
